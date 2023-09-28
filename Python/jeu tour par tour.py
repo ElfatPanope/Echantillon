13c46da62c343ea7a3e02ef0689108614bd16451
ElfatPanope <maxime.ducaruge61260@gmail.com>
2023-09-28 23:13:33 +0200

    1ere étape

---
+++ Python/jeu tour par tour.py
@@ -0,0 +1,6 @@
+Joueur1 = input('Entrez le nom du Joueur1:').capitalize()
+PvJoueur1 = int(input('Entrez le nombre de pv du Joueur1:')),str("Pv")
+Joueur2 = input('Entrez le nom du Joueur2:').capitalize()
+PvJoueur2 = int(input('Entrez le nombre de pv du Joueur2:')),str("Pv")
+
+print(Joueur1,PvJoueur1,Joueur2,PvJoueur2)
