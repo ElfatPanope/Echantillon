e40c65d791c2163905ca533cc790d8ce886b3730
ElfatPanope <maxime.ducaruge61260@gmail.com>
2023-10-02 12:49:15 +0200

    etape2finie

--- Python/jeu tour par tour.py
+++ Python/jeu tour par tour.py
@@ -1,47 +1,57 @@
 Joueur1 = input('Entrez le nom du Joueur1:').capitalize()
-PvJoueur1 = int(input('Entrez le nombre de pv du Joueur1:'))
+Pv1 = int(input('Entrez le nombre de pv du Joueur1:'))
 
 Joueur2 = input('Entrez le nom du Joueur2:').capitalize()
-PvJoueur2 = int(input('Entrez le nombre de pv du Joueur2:'))
+Pv2 = int(input('Entrez le nombre de pv du Joueur2:'))
 
 print()
 
-message = Joueur1 + '(' + str(PvJoueur1) + 'PV) affronte ' + Joueur2 + '(' + str(PvJoueur2) + 'PV)'
+message = Joueur1 + '(' + str(Pv1) + 'PV) affronte ' + Joueur2 + '(' + str(Pv2) + 'PV)'
 print('+' * (len(message)+4))
 print('+',message, '+')
 print('+'*(len(message)+4))
 
 print()
 
-degatJoueur1 =int(input())
-tour1 = Joueur1 + ' , combien infligez-vous de dégats à '+ Joueur2 + ' ? '+ str(degatJoueur1)
-print(tour1)
+att1 =int(input(Joueur1+',combien de dégats infligez-vous à '+ Joueur2 + '?'))
 print()
-Joueur1attck = Joueur1 + ' attaque ' + Joueur2 + ' qui perd ' + str(degatJoueur1) + ' PV'
-print('+'*(len(Joueur1attck)+4))
-print('+',Joueur1attck,'+')
-pvj2=int(PvJoueur2-degatJoueur1)
-vieJoueur2 = Joueur2 + ' a maintenant ' + str(pvj2)+' PV'
-print('+',vieJoueur2,'        +')
-print('+'*(len(Joueur1attck)+4))
+Pv2 -= att1
+msg1 = Joueur1+ ' attaque '+Joueur2+' et lui inflige '+str(att1)+' PV'
+msg2 = Joueur2+' a maintenant '+str(Pv2)+' PV'
+max_size = max(len(msg1), len(msg2))
+msg1 += ' ' * (max_size - len(msg1))
+msg2 += ' ' * (max_size-len(msg2))
+print('+'* (max_size+4))
+print('+', msg1,'+')
+print('+', msg2, '+')
+print('+'*(max_size + 4))
+
 print()
-degatJoueur2 =int(input())
-tour2 = Joueur2 + ' , combien infligez-vous de dégats à '+ Joueur1 + ' ? '+ str(degatJoueur2)
-print(tour2)
+
+att2 =int(input(Joueur2+',combien de dégats infligez-vous à '+ Joueur1 + '?'))
 print()
-Joueur2attck = Joueur2 + ' attaque ' + Joueur1 + ' qui perd ' + str(degatJoueur2) + ' PV'
-print('+'*(len(Joueur2attck)+4))
-print('+',Joueur2attck,'+')
-pvj1=int(PvJoueur1-degatJoueur1)
-vieJoueur1 = Joueur1 + ' a maintenant ' + str(pvj1)+' PV'
-print('+',vieJoueur1,'           +')
-print('+'*(len(Joueur2attck)+4))
+Pv1 -= att2
+msg1 = Joueur2 + ' attaque ' + Joueur1 +' et lui inflige '+ str(att2) +' PV '
+msg2 = Joueur1 + ' a maintenant ' + str(Pv1) +' PV '
+max_size = max(len(msg1), len(msg2))
+msg1 += ' ' * (max_size - len(msg1))
+msg2 += ' ' * (max_size-len(msg2))
+print('+'* (max_size+4))
+print('+', msg1,'+')
+print('+', msg2, '+')
+print('+'*(max_size + 4))
+
 print()
-resultattour1='Résultat du combat'
-print('+'*(len(resultattour1)+4))
-print('+',resultattour1,'+')
-finPVJ1= Joueur1 + ' a '+ str(pvj1)+' PV'
-print('+',finPVJ1,'       +')
-finPVJ2= Joueur2 + ' a '+ str(pvj2)+' PV'
-print('+',finPVJ2,'    +')
-print('+'*(len(resultattour1)+4))
+
+msg1 = ' Résultat du combat : '
+msg2 = Joueur1 +' a '+ str(Pv1)+' PV'
+msg3 = Joueur2 +' a '+ str(Pv2)+' PV'
+max_size = max(len(msg1), len(msg2), len(msg3))
+msg1 += ' ' * (max_size - len(msg1))
+msg2 += ' ' * (max_size-len(msg2))
+msg3 += ' ' * (max_size - len(msg3))
+print('+'* (max_size+4))
+print('+', msg1,'+')
+print('+', msg2, '+')
+print('+', msg3, '+')
+print('+'*(max_size + 4))
